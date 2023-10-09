b2debb94d3a7389e93dc1e1ab3f0f261e8265632
Maxime Coriton <maxime.coriton@gmail.com>
2023-10-09 16:45:40 +0200

    draft

--- Pages/[...slug].vue
+++ Pages/[...slug].vue
@@ -6,7 +6,7 @@
 const { slug } = useRoute().params;
 const story = await useAsyncStoryblok(
   slug && slug.length > 0 ? slug.join("/") : "home",
-  { version: "published" }
+  { version: "draft" }
 );
 
 console.log("Story object:", story);
