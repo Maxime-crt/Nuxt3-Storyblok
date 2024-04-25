096a0d6043add01df1fcf297f75fa060538987c2
Maxime crt <maxime.coriton@gmail.com>
2024-04-25 15:59:52 +0200

    i18n install

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,9 +4,23 @@ export default defineNuxtConfig({
     modules: [
         ["@storyblok/nuxt", {accessToken: process.env.NUXT_STORYBLOK_TOKEN, cacheProvider: 'memory'}],
         '@nuxtjs/tailwindcss',
+        '@nuxtjs/i18n',
     ],
     css: [
         '~/assets/css/styles.css',
     ],
-
+    i18n: {
+       locales: [
+            {
+                code: 'fr',
+                name: 'Français'
+            },
+            {
+                code: 'en',
+                name: 'English'
+            },
+        ],
+        defaultLocale: 'fr',
+        strategy: 'prefix_expect_default',
+    },
 })
